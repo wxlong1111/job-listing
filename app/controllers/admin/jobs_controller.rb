b255f5edbb700002339aa7d02efb9f797cf9251e
wxlong <wxlong1111@163.com>
2017-04-15 12:43:29 +0800

    implement wage part

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -51,7 +51,7 @@ class Admin::JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permin(:title, :description)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email)
   end
 
 end
