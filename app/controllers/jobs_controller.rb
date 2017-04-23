441e4d0718f4f3480ad766f4508cb884483b139d
wxlong <wxlong1111@163.com>
2017-04-23 22:50:51 +0800

    move sqlite3 to dev group & add pg to production group

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,13 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
+  before_action :validate_search_key, only: [:search]
+
+  def search
+      if @query_string.present?
+        search_result = Job.published.ransack(@search_criteria).result(:distinct => true)
+        @jobs = search_result.recent.paginate(:page => params[:page], :per_page => 5 )
+      end
+  end
 
   def index
     @jobs = case params[:order]
@@ -60,7 +68,23 @@ class JobsController < ApplicationController
   private
 
   def job_params
-    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
+    params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden, :category, :company, :city)
   end
+
+  protected
+
+  def validate_search_key
+    @query_string = params[:q].gsub(/\\|\'|\/|\?/, "")
+    if params[:q].present?
+      @search_criteria =  {
+        title_or_company_or_city_cont: @query_string
+      }
+    end
+  end
+
+  def search_criteria(query_string)
+    { :title_cont => query_string }
+  end
+
 
 end
